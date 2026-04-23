9b270e0814a5ae7cbd5377306758acc24605c39e
malakhassan12 <158052165+malakhassan12@users.noreply.github.com>
2026-04-23 15:34:16 +0200

    Malak make build

--- taskflow/vite.config.js
+++ taskflow/vite.config.js
@@ -2,28 +2,53 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { visualizer } from "rollup-plugin-visualizer";
-import vitePluginImp from "vite-plugin-imp";
+// import vitePluginImp from "vite-plugin-imp";
 
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//     vitePluginImp({
+//       libList: [
+//         {
+//           libName: "antd",
+//           style: (name) => `antd/es/${name}/style`,
+//         },
+//       ],
+//     }),
+//     visualizer({ open: true }),
+//   ],
+//   build: {
+//     chunkSizeWarningLimit: 300,
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           "react-vendor": ["react", "react-dom", "react-router-dom"],
+//         },
+//       },
+//     },
+//   },
+// });
+
+// ... بقية الـ imports
 export default defineConfig({
+  base: '/Taskflow_frontend/', // اسم الـ Repo على GitHub
   plugins: [
     react(),
-    tailwindcss(),
-    vitePluginImp({
-      libList: [
-        {
-          libName: "antd",
-          style: (name) => `antd/es/${name}/style`,
-        },
-      ],
-    }),
-    visualizer({ open: true }),
+    tailwindcss(), // ده المسؤول عن تشغيل الستايل بتاع Tailwind
+    visualizer({ open: false }), // خليه false عشان ميفضلش يفتح كل شوية وأنت بتجرب
   ],
   build: {
     chunkSizeWarningLimit: 300,
     rollupOptions: {
       output: {
-        manualChunks: {
-          "react-vendor": ["react", "react-dom", "react-router-dom"],
+        manualChunks(id) {
+          // تقسيم المكتبات الأساسية في ملف لوحدها
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'react-vendor';
+            }
+          }
         },
       },
     },
