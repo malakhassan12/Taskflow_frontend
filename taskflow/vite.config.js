cd6a863be46b0e7b11668424265db8c131628da3
malakhassan12 <158052165+malakhassan12@users.noreply.github.com>
2026-04-23 16:06:47 +0200

    Malak make deploy part 2

--- taskflow/vite.config.js
+++ taskflow/vite.config.js
@@ -30,27 +30,62 @@ import { visualizer } from "rollup-plugin-visualizer";
 //   },
 // });
 
-// ... بقية الـ imports
-export default defineConfig({
-  base: '/Taskflow_frontend/', // اسم الـ Repo على GitHub
-  plugins: [
-    react(),
-    tailwindcss(), // ده المسؤول عن تشغيل الستايل بتاع Tailwind
-    visualizer({ open: false }), // خليه false عشان ميفضلش يفتح كل شوية وأنت بتجرب
-  ],
-  build: {
-    chunkSizeWarningLimit: 300,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // تقسيم المكتبات الأساسية في ملف لوحدها
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
-              return 'react-vendor';
+// // ... بقية الـ imports
+// export default defineConfig({
+//   // base: '/Taskflow_frontend/', // اسم الـ Repo على GitHub
+//   plugins: [
+//     react(),
+//     tailwindcss(), // ده المسؤول عن تشغيل الستايل بتاع Tailwind
+//     visualizer({ open: false }), // خليه false عشان ميفضلش يفتح كل شوية وأنت بتجرب
+//   ],
+//   build: {
+//     chunkSizeWarningLimit: 300,
+//     rollupOptions: {
+//       output: {
+//         manualChunks(id) {
+//           // تقسيم المكتبات الأساسية في ملف لوحدها
+//           if (id.includes('node_modules')) {
+//             if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+//               return 'react-vendor';
+//             }
+//           }
+//         },
+//       },
+//     },
+//   },
+// });
+
+
+
+
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "/Taskflow_frontend/" : "/",
+    
+    plugins: [
+      react(),
+      tailwindcss(), // ده اللي بيخلي ستايل Tailwind يظهر في الـ build
+      visualizer({ open: false }), 
+    ],
+    
+    build: {
+      chunkSizeWarningLimit: 300,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            // تجميع مكتبات React الأساسية في ملف منفصل لتحسين الأداء
+            if (id.includes("node_modules")) {
+              if (
+                id.includes("react") ||
+                id.includes("react-dom") ||
+                id.includes("react-router-dom")
+              ) {
+                return "react-vendor";
+              }
             }
-          }
+          },
         },
       },
     },
-  },
+  };
 });
